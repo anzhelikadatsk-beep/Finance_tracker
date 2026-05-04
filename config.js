3ad9bf429d788fe8c2384a7612452295f6d996e4
anzhelikadatsk-beep <anzhelika.datsk@gmail.com>
2026-05-04 21:56:51 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 // SECRET_TOKEN беремо з PropertiesService у GAS (той самий рядок).
 window.APP_CONFIG = {
   // URL твого Apps Script Web App деплою (закінчується на /exec)
-  API_URL: 'https://script.google.com/macros/s/REPLACE_WITH_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwvGu_HwHD_MeCEJq7j5pis4j6Zbw6002fqyDtOZwxeFw7u0IPbzqhSrBMpeK9Xo3QGfA/exec',
 
   // Той самий SECRET_TOKEN, що в Script Properties Apps Script
-  SECRET_TOKEN: 'REPLACE_WITH_SECRET_TOKEN'
+  SECRET_TOKEN: 'eb7yjlM2Wr53kTnu1I7pWyAB1qYfH7Qi-zbYf1NvAhY'
 };
